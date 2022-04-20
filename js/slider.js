44ba2a4d0bde7ec84a8d412182e2bde3e8181d70
JENNIFER CRISTINA SANTOS DA SILVA <21276662@CFP127ALUNO.LOCAL>
2022-04-20 16:57:50 -0300

    Fazendo o tratamento das funções para deixa-las limpas

--- js/slider.js
+++ js/slider.js
@@ -1,42 +1,58 @@
 'use strict'
 
 const imagens = [
-    {id: '1', url: '../img/zootopia-1.jpg'},
-    {id: '2', url: '../img/zootopia-2.png'},
-    {id: '3', url: '../img/zootopia-3.jpg'},
-    {id: '4', url: '../img/zootopia-4.jpg'},
-    {id: '5', url: '../img/zootopia-5.jpg'}
+    { id: '1', url: '../img/zootopia-1.jpg' },
+    { id: '2', url: '../img/zootopia-2.png' },
+    { id: '3', url: '../img/zootopia-3.jpg' },
+    { id: '4', url: '../img/zootopia-4.jpg' },
+    { id: '5', url: '../img/zootopia-5.jpg' }
 ]
 
-const criarSlider = (imagens) => {
+// Cria span anterior
+const criarAnterior = () => {
 
-    // Cria span anterior
-    const slider = document.querySelector('.slider')
     const anteriorButton = document.createElement('span')
     anteriorButton.classList.add('slider-button')
     anteriorButton.id = 'anterior'
     anteriorButton.innerHTML = '&laquo;'
 
-    // Cria a div das imagens
+    return anteriorButton
+}
+
+// Cria a div das imagens
+const criarSlides = () => {
+
     const sliderContainer = document.createElement('div')
     sliderContainer.classList.add('slider-item-container')
-    const slides = imagens.map( item => `
+    const slides = imagens.map(item => `
     <div class="slider-item">
         <img src="${item.url}">
     </div>
 `)
     sliderContainer.innerHTML = slides.join("")
 
-    // Cria o span proximo
+    return sliderContainer
+}
+
+// Cria o span proximo
+const criarProximo = () => {
+
     const proximoButton = document.createElement('span')
     proximoButton.classList.add('slider-button')
     proximoButton.id = 'proximo'
     proximoButton.innerHTML = '&raquo;'
 
+    return proximoButton
+}
+
+const criarSlider = () => {
+
+    const slider = document.querySelector('.slider')
+
     // Inserido os objetos HTML no DOM (página)
-    slider.appendChild(anteriorButton)
-    slider.appendChild(sliderContainer)
-    slider.appendChild(proximoButton)
+    slider.appendChild(criarAnterior())
+    slider.appendChild(criarSlides())
+    slider.appendChild(criarProximo())
 }
 
 
@@ -56,9 +72,6 @@ const anteriorItem = () => {
     sliderItemContainer.prepend(ultimoItem)
     sliderItems = document.querySelectorAll('.slider-item')
 }
-
-
-
 
 setInterval(() => {
     proximoItem()
